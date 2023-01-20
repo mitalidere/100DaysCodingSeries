27a4dd71e7c414a3d4cbd5e2fecfc12e105bc333
mitalidere <121154247+mitalidere@users.noreply.github.com>
2023-01-20 18:22:46 +0530

    Day 3

---
+++ Day3.java
@@ -0,0 +1,11 @@
+// Write a program to find ASCII values of a character.
+
+import java.util.Scanner;
+
+public class Day3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char input=sc.next().charAt(0);
+        System.out.println((int)input);
+    }
+}
